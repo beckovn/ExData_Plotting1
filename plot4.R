4f8e949b8c2122a07af290e4e65d2ac6527e4d4a
beckovn <69743184+beckovn@users.noreply.github.com>
2021-02-08 17:57:28 +0200

    Add files via upload
    Added legend to the 3-rd subplot

--- plot4.R
+++ plot4.R
@@ -17,13 +17,23 @@ df$DateTime <- with(df, ymd(df$Date)+hms(df$Time))
 # Drawing 4 miniplots by row
 png("plot4.png")
 par(mfrow = c(2,2))
+
+# plot1
 plot(df$DateTime, df$Global_active_power,
      type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+
+# plot2
 plot(df$DateTime, df$Voltage,
      type = "l", xlab = "datetime", ylab = "Voltage")
+
+#plot3
 matplot(df$DateTime, cbind(df$Sub_metering_1, df$Sub_metering_2, df$Sub_metering_3),
         type="l", xlab = "", ylab = "Energy sub metering", col = c("black", "red", "blue"),
         lty=1)
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col = c("black", "red", "blue"), lty = 1)
+
+# plot4
 matplot(df$DateTime, df$Global_reactive_power, type="l", xlab = "datetime",
         ylab = "Global_reactive_power", lty = 1)
 dev.off()
